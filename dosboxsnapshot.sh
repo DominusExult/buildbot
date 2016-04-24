19656d888779201d2e23e394fc710478e19e227f
DominusExult <domiman+github@gmail.com>
2016-04-24 12:21:00 +0200

    Added scripts for DOSBox and xU4. The xU4 script needs more work

---
+++ dosboxsnapshot.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+#functions
+. ./functions.sh
+
+headermain DOSBOX
+
+
+cd ~/Code/snapshots/dosbox
+/usr/bin/svn update --depth=infinity 2> >(teelog >&2) || error SVN update
+
+#configure options for all arches
+CONF_OPT='-q --disable-sdltest --disable-alsatest --enable-core-inline'
+
+#i386
+header i386
+ARCH=i386
+SDK=10.6
+DEPLOYMENT=10.5
+flags
+gcc oldgcc
+autogen
+CONF_ARGS="--prefix=/opt/$ARCH"
+{
+	config
+	patch -p0 -i intel.diff
+	makes
+	/usr/bin/strip ./src/dosbox -o ./src/dosbox_i386 ||  error $ARCH strip
+} 2>&1 | teelog ; pipestatus || return
+
+make -s distclean > /dev/null
+
+#ppc
+header PPC
+ARCH=ppc
+SDK=10.5
+DEPLOYMENT=10.4
+flags
+gcc oldgcc
+autogen
+CONF_ARGS="--prefix=/opt/$ARCH"
+{
+	config
+	patch -p0 -i ppc.diff
+	makes
+	strip ./src/dosbox -o ./src/dosbox_ppc
+} 2>&1 | teelog -a ; pipestatus || return
+
+#deploy
+deploy
+{
+	# make fat build
+	lipo -create -arch i386 ./src/dosbox_i386 -arch ppc ./src/dosbox_ppc -output ./src/DOSBox  ||  error lipo
+
+	# bundle
+	cp ./src/DOSBox ./src/dosboxsvn.app/contents/MacOS/DOSBox ||  error bundle
+
+	# codesign to satisfy OS X 10.8 Gatekeeper
+	codesign --deep --force --sign "Developer ID Application" ./src/dosboxsvn.app ||  error codesign
+
+
+	#make disk image
+	mkdir DOSBox-Snapshot
+	CpMac -r  ./src/dosboxsvn.app ./DOSBox-Snapshot
+	cp ./AUTHORS ./DOSBox-Snapshot/Authors
+	cp ./COPYING ./DOSBox-Snapshot/License
+	cp ./NEWS ./DOSBox-Snapshot/News
+	cp ./README ./DOSBox-Snapshot/ReadMe
+	SetFile -t ttro -c ttxt ./DOSBox-Snapshot/Authors
+	SetFile -t ttro -c ttxt ./DOSBox-Snapshot/License
+	SetFile -t ttro -c ttxt ./DOSBox-Snapshot/News
+	SetFile -t ttro -c ttxt ./DOSBox-Snapshot/ReadMe
+	REVISION=" r$(/usr/bin/svnversion)"
+	hdiutil create -ov -format UDZO -imagekey zlib-level=9 -fs HFS+ -srcfolder DOSBox-snapshot -volname "DOSBox SVN snapshot$REVISION" Dosbox-Snapshot.dmg || error disk image
+
+	# copy app to applications and file the snapshots
+	cp -R ./src/DOSBoxSVN.app /Applications/
+	cp -p Dosbox-Snapshot.dmg ~/Snapshots/dosbox/"`date +%y-%m-%d-%H%M` DOSBox$REVISION.dmg"
+	mv Dosbox-Snapshot.dmg ~/Snapshots/dosbox/
+
+	# "upload"
+	cp -p ~/Snapshots/dosbox/Dosbox-Snapshot.dmg ~/dropbox/public/dosbox/
+} 2>&1 | teelog -a ; pipestatus || return
+
+
+# cleanup
+make -s distclean > /dev/null
+rm -r DOSBox-Snapshot
---
+++ xu4snapshot.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+#functions
+. ./functions.sh
+
+headermain XU4
+
+cd ~/code/snapshots/xu4
+/usr/bin/svn update --depth=infinity || error SVN update
+cd src
+
+#x86_64
+ARCH=x86_64
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+export CFLAGS=
+export LDFLAGS=
+export CXXFLAGS=$CFLAGS
+export CPPFLAGS=$CXXFLAGS
+export ACLOCAL_FLAGS=
+export PKG_CONFIG_PATH=
+export SPECIFIC_ARCH=
+export SYSROOT='/opt/SDKs/MacOSX10.10.sdk -mmacosx-version-min=10.7'
+make -f makefile.osx clean && make -f makefile.osx clean-local > /dev/null
+make -j9 -s -f makefile.osx || error $ARCH make
+strip u4 -o u4_$ARCH || error $ARCH strip
+
+#i386
+ARCH=i386
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+export CFLAGS= "-w"
+export CXXFLAGS=$CFLAGS
+export CPPFLAGS=$CXXFLAGS
+export LDFLAGS=
+export PKG_CONFIG_PATH=
+export SPECIFIC_ARCH='-arch i386'
+export SYSROOT='/opt/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5'
+make -f makefile.osx clean > /dev/null
+make -j9 -s -f makefile.osx || error $ARCH make
+strip u4 -o u4_$ARCH || error $ARCH strip
+
+#ppc
+ARCH=ppc
+export PATH=/opt/xcode3/usr/bin:/opt/xcode3/usr/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+export CC=/opt/xcode3/usr/bin/gcc-4.2
+export CXX=/opt/xcode3/usr/bin/g++-4.2
+export CFLAGS= "-w"
+export CXXFLAGS=$CFLAGS
+export CPPFLAGS=$CXXFLAGS
+export LDFLAGS=
+export PKG_CONFIG_PATH=
+export SPECIFIC_ARCH='-arch ppc'
+export SYSROOT='/opt/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.4'
+make -f makefile.osx clean > /dev/null
+make -j9 -s -f makefile.osx || error $ARCH make
+strip u4 -o u4_$ARCH || error $ARCH strip
+
+#make fat binary
+lipo -create -arch x86_64 u4_x86_64 -arch i386 u4_i386 -arch ppc u4_ppc -output u4 || error lipo
+
+#bundle
+make -f makefile.osx bundle || error bundle
+
+#codesign to satisfy OS X 10.8 Gatekeeper
+codesign --force --sign "Developer ID Application" xu4.app/contents/macos/u4 || error codesign
+
+#little hack to prevent the next step from restoring the unsigned binary in the bundle
+cp xu4.app/contents/macos/u4 u4
+
+#image
+export REVISION=" r$(/usr/bin/svnversion)"
+make -f makefile.osx osxdmg || error disk image
+
+# copy the disk image to the snapshots storage path with date and time stamp
+# to be able to easily trace back regressions
+cp -p xu4-MacOSX.dmg ~/Snapshots/xu4/"`date +%y-%m-%d-%H%M` xu4$REVISION.dmg"
+
+# move the disk image to the snapshots storage - this will be overwritten each 
+# time you make a snapshot
+mv xu4-MacOSX.dmg ~/Snapshots/xu4/
+rm -r /applications/xu4.app
+cp -R xu4.app /Applications/
+
+
+# if you have uploaded your public key to SF you can automatically upload the 
+# snapshot to the download page
+scp -p -i ~/.ssh/id_dsa ~/Snapshots/xu4/xu4-MacOSX.dmg $USER,xu4@web.sourceforge.net:web/download/xu4-MacOSX.dmg || error Upload
+
+#leaving everything clean
+make -f makefile.osx clean > /dev/null && make -f makefile.osx clean-local > /dev/null
